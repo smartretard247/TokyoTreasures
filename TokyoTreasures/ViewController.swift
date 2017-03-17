4ea6e483aa715a131fe709b2cc40728c18635a52
Jeezy <Jeezy@Jeezys-MacBook-Air.local>
2017-03-17 15:09:10 +0900

    Initial Commit

---
+++ TokyoTreasures/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TokyoTreasures
+//
+//  Created by Jeezy on 3/17/17.
+//  Copyright © 2017 Simply Silver AKY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
